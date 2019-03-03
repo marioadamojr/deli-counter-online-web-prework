ca5692b53ba14183e352c3b271098966d72890c6
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-03 20:30:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(deli)
-  if deli == []
+  if deli.length == []
     puts "The line is currently empty."
   else
     deli.each_with_index do |name, index|
@@ -8,6 +8,7 @@ def line(deli)
     end
     puts line
   end
+end
 
 
 def take_a_number(deli, customer)
